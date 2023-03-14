aea9bf184624729aa340568c6cee90ccac417821
forhyeonji <ghkdguswl3@gmail.com>
2023-03-14 22:59:44 +0900

    CSS

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import styles from './AddTodo.module.css';
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState('');
@@ -19,14 +20,15 @@ export default function AddTodo({ onAdd }) {
     setText('');
   };
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
+        className={styles.input}
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleChange}
       />
-      <button>Add</button>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
